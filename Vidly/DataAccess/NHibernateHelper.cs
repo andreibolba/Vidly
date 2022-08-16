9609bd63c9ff4004a2a8dfcf66764bb336820f2e
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2022-08-16 10:39:34 +0300

    Add te DataAccessLayer for configure of NHibernate

---
+++ Vidly/DataAccess/NHibernateHelper.cs
@@ -0,0 +1,41 @@
+﻿using NHibernate;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate.Tool.hbm2ddl;
+
+namespace Vidly.Models.NHibernate
+{
+    public class NHibernateHelper
+    {
+        private static ISessionFactory _sessionFactory;
+
+        private static ISessionFactory SessionFactory
+        {
+            get
+            {
+                if (_sessionFactory == null)
+                    InitializeSessionFactory(); return _sessionFactory;
+            }
+        }
+
+        private static void InitializeSessionFactory()
+        {
+            _sessionFactory=Fluently.Configure().Database(MsSqlConfiguration.MsSql2008
+                .ConnectionString("Data Source=andreibo-mbl;Initial Catalog=VidlyStore;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework"))
+                .Mappings(m => m.FluentMappings
+                .AddFromAssemblyOf<CostumerHibernate>())
+                .ExposeConfiguration(cfg => new SchemaExport(cfg)
+                .Create(true, true))
+                .BuildSessionFactory();
+        }
+
+        public static ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+    }
+}
