3de98e66ee8c90b941e85518c1ff3ee71941c4f5
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2022-08-16 10:38:56 +0300

    Add classes for Customer and MembershipType for NHibernate

---
+++ Vidly/Models/NHibernate/CostumerHibernate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models.NHibernate
+{
+    public class CostumerHibernate
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual bool IsSubscribedToNewsletter { get; set; }
+        public virtual MembershipType MembershipType { get; set; }
+        public virtual byte MembershipTypeId { get; set; }
+        public virtual DateTime? BirthDate { get; set; }
+    }
+}
---
+++ Vidly/Models/NHibernate/MembershipTypeHibernate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models.NHibernate
+{
+    public class MembershipTypeHibernate
+    {
+        public virtual byte Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual short SignUpFee { get; set; }
+        public virtual byte DurationInMonth { get; set; }
+        public virtual byte Discount { get; set; }
+
+    }
+}
